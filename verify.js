477ab201a414890adffbbd36bc2adb8e01c7b510
methu45 <110477491+methu45@users.noreply.github.com>
2022-08-03 13:26:48 +0530

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,8 +1,8 @@
 const exec = require('child_process').exec;
 const os = require("os");
 async function startdiana() {
-  var sdn = 'RUN git  https://github.com/diabykavi/queendiabykaviaah /root/queendiana' + '\n'
-  exec('sed -n 3p /root/queendiana/queendiabykaviaah/Dockerfile', async (err, stdout, stderr) => {
+  var sdn = 'RUN git  https://github.com/methu45/VX /root/queendiana' + '\n'
+  exec('sed -n 3p /root/queendiana/VX/Dockerfile', async (err, stdout, stderr) => {
     if (sdn !== stdout) {
       throw new Error("😒 Fake User Found •• Only For QUEEN DIANA-Bot Users !!");
     }
