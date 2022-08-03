1701548e956faca50c4acb750ab491fcafe66df5
methu45 <110477491+methu45@users.noreply.github.com>
2022-08-03 17:04:45 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+{
+    "APIs": {
+      "zenz": "https://zenzapis.xyz",
+      "apikey": "YOURAPIKEY"
+    },
+    "options": {
+      "autoRead": true,
+      "self": false,
+      "limitCount": 25,
+      "limitGame": 30
+    },
+    "exif": {
+      "packname": "zenzapis",
+      "author": "Axzi"
+    },
+    "owner": [
+      "94766598862",
+      "94785715875"
+    ],
+    "timezone": "Asia/Colombo",
+    "footer": "©Axzi"
+}
